db8ea7ab3769385e313c03e7acae04077c5f7810
Michal <michal.wierzbinski@gmail.com>
2020-10-26 14:48:20 +0000

    Added test for Android

--- common/src/commonMain/kotlin/com/example/kotlinmultiplatformperformance/common/ThreadPerformance.kt
+++ common/src/commonMain/kotlin/com/example/kotlinmultiplatformperformance/common/ThreadPerformance.kt
@@ -28,11 +28,21 @@ class ThreadPerformance {
     }
     fun singleTaskOnMultipleThreads() {
         // For running compute on multiple background threads
-
+        // Not sure if that is working correctly atm. We hope to spawn multiple threads and work on the task in parallel
         for (currentIndex in 0 until 5) {
             GlobalScope.launch {
                 // heavy operation here that returns a Result
                 compute()
+            }
+        }
+    }
+
+    fun multipleTaskOnSingleBackgroundThread() {
+        // For running compute multiple times on background thread
+        // Not sure if that is working correctly atm. We would like to start multi[le tasks on a single background thread
+        GlobalScope.launch {
+            for (currentIndex in 0 until 5) {
+                 compute()
             }
         }
     }
@@ -58,5 +68,4 @@ class ThreadPerformance {
         }
         return sorted
     }
-
 }
